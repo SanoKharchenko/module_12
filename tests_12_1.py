fc392e44b27adc63a1719caf772dc4824590f5fe
Aleksandr <alex.kharchenko@mail.ru>
2024-09-10 14:19:36 +0300

    Add files via upload

--- tests_12_1.py
+++ tests_12_1.py
@@ -3,18 +3,23 @@ import runner
 
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         run = runner.Runner('test_walk')
         for i in range(10):
             run.walk()
         self.assertEqual(run.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         run = runner.Runner('test_run')
         for i in range(10):
             run.run()
         self.assertEqual(run.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         run_1 = runner.Runner('tester_1')
         run_2 = runner.Runner('tester_2')
