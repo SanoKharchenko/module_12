9a95d75a2ee5a23d67a7f9f9428920c44f502b17
Aleksandr <alex.kharchenko@mail.ru>
2024-09-10 14:19:55 +0300

    Add files via upload

--- tests_12_2.py
+++ tests_12_2.py
@@ -3,11 +3,13 @@ import runner_and_tournament
 
 
 class TournamentTest(unittest.TestCase):
+    is_frozen = True
 
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def setUp(self):
         self.runner_1 = runner_and_tournament.Runner("Усэйн", 10)
         self.runner_2 = runner_and_tournament.Runner("Андрей", 9)
@@ -18,6 +20,7 @@ class TournamentTest(unittest.TestCase):
         for result in cls.all_results.values():
             print({key: str(runner) for key, runner in result.items()})
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_1(self):
         tour1 = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3)
         result1 = tour1.start()
@@ -25,6 +28,7 @@ class TournamentTest(unittest.TestCase):
         self.assertTrue(last_runner1[-1] == 'Ник')
         self.all_results['test_1'] = result1
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_2(self):
         tour2 = runner_and_tournament.Tournament(90, self.runner_2, self.runner_3)
         result2 = tour2.start()
@@ -32,6 +36,7 @@ class TournamentTest(unittest.TestCase):
         self.assertTrue(last_runner2[-1] == 'Ник')
         self.all_results['test_2'] = result2
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_3(self):
         tour3 = runner_and_tournament.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
         result3 = tour3.start()
