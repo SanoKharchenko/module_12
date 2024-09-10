4d477cdbac161a8998089c40364275e06f77fc0d
Aleksandr <alex.kharchenko@mail.ru>
2024-09-10 14:01:33 +0300

    Add files via upload

---
+++ tests_12_2.py
@@ -0,0 +1,44 @@
+import unittest
+import runner_and_tournament
+
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner_1 = runner_and_tournament.Runner("Усэйн", 10)
+        self.runner_2 = runner_and_tournament.Runner("Андрей", 9)
+        self.runner_3 = runner_and_tournament.Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results.values():
+            print({key: str(runner) for key, runner in result.items()})
+
+    def test_1(self):
+        tour1 = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3)
+        result1 = tour1.start()
+        last_runner1 = list(result1.values())
+        self.assertTrue(last_runner1[-1] == 'Ник')
+        self.all_results['test_1'] = result1
+
+    def test_2(self):
+        tour2 = runner_and_tournament.Tournament(90, self.runner_2, self.runner_3)
+        result2 = tour2.start()
+        last_runner2 = list(result2.values())
+        self.assertTrue(last_runner2[-1] == 'Ник')
+        self.all_results['test_2'] = result2
+
+    def test_3(self):
+        tour3 = runner_and_tournament.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
+        result3 = tour3.start()
+        last_runner3 = list(result3.values())
+        self.assertTrue(last_runner3[-1] == 'Ник')
+        self.all_results['test_3'] = result3
+
+
+if __name__ == '__main__':
+    unittest.main()
